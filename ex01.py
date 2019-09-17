22d3eadc20150f9ef6ac8e7fa24d4b4198d1d5ae
nelma <nelmanonato@gmail.com>
2019-09-16 22:15:19 -0300

    primeiro commit

---
+++ ex01.py
@@ -0,0 +1,57 @@
+idade = int(input('Digite a idade'))
+tem habilitacao =(input('Digite Sim'))
+if idade>=18 and tem habilitacao ==('Informe'):
+    print('Pode dirigir')
+else:
+    print('Nao pode dirigir')
+
+
+
+exit()
+idade = int(input('Digite a idade '))
+if idade>=18:
+    print('Pode dirigir')
+else:
+    print('1Não pode dirigir')
+        
+habilitação =(input('Digite Sim'))
+if habilitação == "Sim":
+        print('pode dirigir')     
+else:
+        print("2Não pode dirigir")
+
+
+exit()
+N1 = int(input('Digite o primeiro numero'))
+N2 = int(input('Digite o segundo numero'))
+if N1>N2:
+    print('N1 é maior')
+else:
+    print("N2 é maior")
+
+
+exit()
+num1 = int(input("Digite o primeiro numero"))
+print(minor)
+
+
+
+exit()
+num1 = int(input("Digite o primeiro numero"))
+num2 = int(input("Digite o segundo numero"))
+major = num1>num2
+print(major)
+
+exit()
+num1 = int(input("Digite o primeiro numero"))
+num2 = int(input("digite o segundo numero"))
+equal = num1==num2
+print(equal)
+
+
+
+exit()
+num1 = int(input("Digite o primeiro numero"))
+num2 = int(input("Digite o segundo numero"))
+soma = num1+num2
+print(soma)
